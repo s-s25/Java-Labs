58ac716ebaf7366168bdc0adaee42749b7fd8825
Stephen Schmidt <106401458+s-s25@users.noreply.github.com>
2024-10-15 18:22:07 +0000

    updated paths

--- Pancake/src/Main.java
+++ Pancake/src/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
-        File file = new File("/home/stephens/Documents/IdeaProjects/Pancake/src/commands.txt");
+        File file = new File("/workspaces/Java-Labs/Pancake/src/commands.txt");
         Robot robot = new Robot();
         robot.run(file);
     }
